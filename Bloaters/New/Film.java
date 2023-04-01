d08b6d7f4eb8f54902835ec3c83ea464b0536592
Mahesa Insan <mahesainsan@gmail.com>
2023-04-01 22:39:20 +0700

    final

---
+++ Bloaters/New/Film.java
@@ -0,0 +1,18 @@
+package Bloaters.New;
+
+public class Film {
+    String filmName, filmDuration, filmRating;
+
+    public void setFilmName(String filmName) {
+        this.filmName = "The Batman";
+    }
+
+    public void setFilmDuration(String filmDuration) {
+        this.filmDuration = "176 Minutes";
+    }
+
+    public void setFilmRating(String filmRating) {
+        this.filmRating = "9 out of 10 stars";
+    }
+}
+
---
+++ Bloaters/Old/Film.java
@@ -0,0 +1,14 @@
+package Bloaters.Old;
+
+public class Film{
+    
+    public static void main(String[] args) {
+        String[] filmDetail = new String[3];
+
+        filmDetail[0] = "The Batman";
+        filmDetail[1] = "176 Minutes";
+        filmDetail[3] = "9 out of 10 stars";
+    }
+
+}
+
